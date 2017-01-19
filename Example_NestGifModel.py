52fa0ff70afb2143c947de4a2cd0a597397dd432
mjaquier <mjaquier@me.com>
2017-01-19 23:22:33 +0100

    Examples

---
+++ Example_NestGifModel.py
@@ -0,0 +1,114 @@
+import nest
+import nest.voltage_trace
+import os
+import numpy as np
+import nest.raster_plot
+import matplotlib.pyplot as plt
+import cPickle as pickle
+
+
+class NestModel:
+    def __init__(self):
+
+        self.name = self.__class__.__name__
+        self.built = False
+        self.connected = False
+
+        # Model Fit Parameters
+        self.neuron_params = {}
+
+        # NEST Model Parameters
+        self.neurons = 50
+        self.p_ex = 0.03
+        self.w_ex = 60.0
+        self.threads = 1
+        self.poisson_neurons = 5  # size of Poisson group
+        self.rate_noise = 5.0  # firing rate of Poisson neurons (Hz)
+        self.w_noise = 10.0  # synaptic weights from Poisson to population
+        self.dt = 0.1
+        self.simtime = 20000
+
+        # Misc
+        self.name = self.__class__.__name__
+        self.data_path = self.name + "/"
+        if not os.path.exists(self.data_path):
+            os.makedirs(self.data_path)
+        print("Writing data to: {0}".format(self.data_path))
+        nest.ResetKernel()
+        nest.SetKernelStatus({"data_path": self.data_path})
+        nest.SetKernelStatus({"resolution": self.dt})
+
+    def set_model_params(self):
+        param_dict= pickle.load(open(
+            "/Users/mj/Documents/NEST/gif_model/NestModel/param/NESTParas.p",
+            "rb"))
+        param_dict['model']['C_m'] *= 10 ** 3
+        self.neuron_params = param_dict['model']
+        print self.neuron_params
+
+    def calibrate(self):
+        """
+        Compute all parameter dependent variables of the
+        model.
+        """
+        self.set_model_params()
+
+        nest.SetKernelStatus({"print_time": True,
+                              "local_num_threads": self.threads,
+                              "resolution": self.dt})
+
+    def build(self):
+        """
+        Create all nodes, used in the model.
+        """
+        if self.built:
+            return
+        self.calibrate()
+
+        self.population = nest.Create("gif_psc_exp", self.neurons,
+                                      params=self.neuron_params)
+        self.noise = nest.Create("poisson_generator", self.poisson_neurons,
+                                 params={'rate': self.rate_noise})
+        self.spike_det = nest.Create("spike_detector")
+        self.voltmeter = nest.Create("voltmeter")
+
+        self.built = True
+
+    def connect(self):
+        """
+        Connect all nodes in the model.
+        """
+        if self.connected:
+            return
+        if not self.built:
+            self.build()
+
+        nest.Connect(self.population, self.population,
+                     {'rule': 'pairwise_bernoulli',
+                      'p': self.p_ex},
+                     syn_spec={"weight": self.w_ex})
+
+        nest.Connect(self.noise, self.population, 'all_to_all',
+                     syn_spec={"weight": self.w_noise})
+
+        nest.Connect(self.population, self.spike_det)
+        nest.Connect(self.voltmeter, self.population)
+        nest.SetStatus(self.voltmeter, [{"withgid": True}])
+
+        self.connected = True
+
+    def run(self):
+        """
+        Simulate the model for simtime milliseconds and print the
+        firing rates of the network during htis period.
+        """
+        if not self.connected:
+            self.connect()
+        nest.Simulate(self.simtime)
+        nest.voltage_trace.from_device(self.voltmeter)
+        nest.raster_plot.from_device(self.spike_det, hist=True)
+        plt.title('Population dynamics')
+        plt.show()
+        print(self.neuron_params)
+
+NestModel().run()
---
+++ Example_modelfit.py
@@ -0,0 +1,136 @@
+import sys
+
+PATH_FLAT = '/Users/mj/Documents/NEST/'
+sys.path.append(PATH_FLAT + '/GIFFittingToolbox/src')
+sys.path.append(PATH_FLAT + '/GIFFittingToolbox/src/HBP')
+
+import loading
+import numpy as np
+import cPickle as pickle
+
+from Experiment import *
+from GIF import *
+from AEC_Badel import *
+from AEC_Dummy import *
+from Filter_Rect_LinSpaced import *
+from Filter_Rect_LogSpaced import *
+from Filter_Exps import *
+from GIF_HT import *
+
+import seaborn
+
+
+class Fit():
+    def __init__(self):
+        self.V_units = 10 ** -3
+        self.I_units = 10 ** -9
+        self.trainData = []
+        self.testData = []
+        self.DT_beforespike = 5.0
+        self.T_ref = 4.0
+        self.tau_gamma = [10.0, 50.0, 250.0]
+        self.eta_tau_max = 1000.0
+        self.tau_opt = []
+        self.eta = []
+        self.gamma = []
+
+
+    @staticmethod
+    def chunks(l, n):
+        """Yield successive n-sized chunks from l."""
+        for i in xrange(0, len(l), n):
+            yield l[i:i + n]
+
+    def importdata(self):
+        # Data[0] = Voltage, Data[1] = Current, Data[2] = Time
+
+        self.trainData, self.testData = loading.Loader().dataload()
+
+        self.myExp = Experiment('Experiment 1', .1)
+        for voltage, current, duration in zip(self.trainData[0],
+                                              self.trainData[1],
+                                              self.trainData[2]):
+            self.myExp.addTrainingSetTrace(voltage, self.V_units,
+                                           current, self.I_units,
+                                           np.size(duration) / 10,
+                                           FILETYPE='Array')
+
+        for voltage, current, duration in zip(self.testData[0],
+                                              self.testData[1],
+                                              self.testData[2]):
+                self.myExp.addTestSetTrace(voltage, self.V_units,
+                                           current, self.I_units,
+                                           np.size(duration) / 10,
+                                           FILETYPE='Array')
+
+        self.fitaec(self.myExp)
+
+    def fitaec(self, myExp):
+        myAEC = AEC_Dummy()
+        myExp.setAEC(myAEC)
+        myExp.performAEC()
+
+        self.optimizetimescales(myExp)
+
+    def optimizetimescales(self, myExp):
+        # myExp.plotTrainingSet()
+        # myExp.plotTestSet()
+
+        myGIF_rect = GIF(0.1)
+
+        myGIF_rect.Tref = self.T_ref
+        myGIF_rect.eta = Filter_Rect_LogSpaced()
+        myGIF_rect.eta.setMetaParameters(length=500.0, binsize_lb=2.0,
+                                         binsize_ub=100.0, slope=4.5)
+        myGIF_rect.fitVoltageReset(myExp, myGIF_rect.Tref, do_plot=False)
+        myGIF_rect.fitSubthresholdDynamics(myExp,
+                                           DT_beforeSpike=self.DT_beforespike)
+
+        myGIF_rect.eta.fitSumOfExponentials(3, [1.0, 0.5, 0.1],
+                                            self.tau_gamma, ROI=None, dt=0.1)
+        print "Optimal timescales: ", myGIF_rect.eta.tau0
+
+        self.tau_opt = [t for t in myGIF_rect.eta.tau0 if t < self.eta_tau_max]
+
+        self.fitmodel(myExp)
+
+    def fitmodel(self, myExp):
+        myGIF = GIF(0.1)
+        myGIF.Tref = self.T_ref
+        myGIF.eta = Filter_Exps()
+        myGIF.eta.setFilter_Timescales(self.tau_opt)
+        myGIF.gamma = Filter_Exps()
+        myGIF.gamma.setFilter_Timescales(self.tau_gamma)
+        myGIF.fit(myExp, DT_beforeSpike=self.DT_beforespike)
+        myPrediction = myExp.predictSpikes(myGIF, nb_rep=500)
+        Md = myPrediction.computeMD_Kistler(4, 0.1)
+        myPrediction.plotRaster(delta=1000.0)
+
+        self.eta = myGIF.eta.getCoefficients()
+        self.gamma = myGIF.gamma.getCoefficients()
+        myGIF.plotParameters()
+        myGIF.save('myGIF.pck')
+
+        self.model_params(myGIF)
+
+    def model_params(self, gif):
+        q_stc = []
+        q_sfa = []
+        res_dic = gif.getResultDictionary()
+        pickle.dump(res_dic, open("NestModel/param/GIFParas.p", "wb"))
+        for eta_index, eta in enumerate(res_dic['model']['q_stc']):
+            q_eta_temp = eta / (
+                1 - np.exp(-self.T_ref /
+                           res_dic['model']['tau_stc'][eta_index]))
+            q_stc.append(q_eta_temp)
+        for gamma_index, gamma in enumerate(res_dic['model']['q_sfa']):
+            q_gamma_temp = gamma / (
+                1 - np.exp(-self.T_ref /
+                           res_dic['model']['tau_sfa'][gamma_index]))
+            q_sfa.append(q_gamma_temp)
+        res_dic['model']['q_stc'] = q_stc
+        res_dic['model']['q_sfa'] = q_sfa
+
+
+        pickle.dump(res_dic, open("NestModel/param/NESTParas.p", "wb"))
+Fit().importdata()
